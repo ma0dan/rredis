ef920208d6281dd8347853b9034caf80ae0c02a1
chad <chad@selftalk.me>
2026-07-31 15:30:17 +0800

    done 02

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import sys
 
-def handle_command(args):
-    """Process a Redis command and return the RESP response."""
-    cmd = args[0].upper()
 
-    if cmd == "PING":
-        # TODO: Return +PONG\r\n for no args
-        # TODO: Return bulk string for PING <message>
-        if len(args) == 1:
-            return "+PONG\r\n"
-        else:
-            msg = " ".join(args[1:])
-            return f"${len(msg)}\r\n{msg}\r\n"
+def bulk_string(args):
+    body = ' '.join(args)
+    length = len(body)
+    return f"${length}\r\n{body}\r\n"
+
+def cmd_ping(args):
+    """Process a Redis command and return the RESP response."""
+    if args:
+        return bulk_string(args)
+    return "+PONG\r\n"
+
+def cmd_echo(args):
+    if args:
+        return bulk_string(args)
+
+HANDLERS = {'PING': cmd_ping, 'ECHO': cmd_echo}
+
+def handle_command(args):
+    cmd = args[0].upper()
+    handler = HANDLERS.get(cmd)
+    if handler:
+        return handler(args[1:])
     return "-ERR unknown command\r\n"
+
 
 def main():
     for line in sys.stdin:
